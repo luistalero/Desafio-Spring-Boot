5bb6a2177cd61e35ff9e4b4887fac21b3c506749
luistalero <luisalbertotaleromartinez@gmail.com>
2025-04-29 16:19:40 -0500

    feat: :tada: java and spring boot project initialization the project to make the creation of a database in postgres from java is started.

---
+++ src/main/java/com/schooltibu/schooltibu/SchooltibuApplication.java
@@ -0,0 +1,13 @@
+package com.schooltibu.schooltibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooltibuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooltibuApplication.class, args);
+	}
+
+}
